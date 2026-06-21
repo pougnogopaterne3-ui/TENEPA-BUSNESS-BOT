9d19eee9f0ef6ba1043dba98f7a853724d2dc555
pougnogopaterne3-ui <pougnogopaterne3@gmail.com>
2026-06-21 12:08:40 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "243833389567" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "22667961869" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
